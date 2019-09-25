9d7c91a4e27473b1aa9e63551ede8f6df7fe253e
daniel <g.thirteen333@gmail.com>
2019-09-24 20:39:58 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const morgan = require('morgan');
+
+const app = express();
+app.use(morgan('tiny'));
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello world' })
+})
+
+app.listen('1337');
